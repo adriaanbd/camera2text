ef34c9c48c8285e4fafb521b5281afee0760b858
ElianaArjona <elianaarjonab@gmail.com>
2020-08-09 18:38:53 -0500

    get position selected in rectangle

--- drawMouse.js
+++ drawMouse.js
@@ -1,16 +1,15 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-var rect = {};
-var drag = false;
-var imageObj = null;
+var position = []
+
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d'),
+    rect = {},
+    drag = false;
+
 
 function init() {
     imageObj = new Image();
-    imageObj.onload = function () { 
-        ctx.drawImage(imageObj, 0, 0,500,500); 
-    };
+    imageObj.onload = function () { ctx.drawImage(imageObj, 0, 0,500,500); };
     imageObj.src = './static/dog-cat.png';
-
     canvas.addEventListener('mousedown', mouseDown, false);
     canvas.addEventListener('mouseup', mouseUp, false);
     canvas.addEventListener('mousemove', mouseMove, false);
@@ -22,17 +21,32 @@ function mouseDown(e) {
     drag = true;
 }
 
-function mouseUp() { drag = false; }
+function mouseUp() {
+    drag = false;
+    console.log(position)
+}
 
 function mouseMove(e) {
     if (drag) {
-        ctx.clearRect(0, 0, 500, 500);
-        ctx.drawImage(imageObj, 0, 0,500,500);
-        rect.w = (e.pageX - this.offsetLeft) - rect.startX;
-        rect.h = (e.pageY - this.offsetTop) - rect.startY;
-        ctx.strokeStyle = 'red';
-        ctx.strokeRect(rect.startX, rect.startY, rect.w, rect.h);
+      rect.w = (e.pageX - this.offsetLeft) - rect.startX;
+      rect.h = (e.pageY - this.offsetTop) - rect.startY ;
+      ctx.clearRect(0,0,canvas.width,canvas.height);
+      ctx.drawImage(imageObj, 0, 0,500,500);
+      position = draw();
     }
 }
-//
+
+function draw() {
+    ctx.beginPath();
+    ctx.rect(rect.startX, rect.startY, rect.w, rect.h);
+    ctx.strokeStyle = 'blue';
+    ctx.lineWidth = 3;
+    ctx.stroke();
+    return [rect.startX, rect.startY, rect.w, rect.h];
+}
+
+
+
+
 init();
+console.log(position);
